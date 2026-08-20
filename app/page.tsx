4b0fd25199d9bad699c59073c5529a1609c2d27b
Rimy Akter <asha86081@gmail.com>
2026-08-21 00:59:53 +0600

    Add greeting message to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Input } from "@/components/ui/input";
+
 
 export default function Home() {
   return (
     <div>
       <p className="text-lg font-bold text-center pt-12">Hello Production !!!</p>
+      <p className="text-center">Next js</p>
     </div>
   );
 }
